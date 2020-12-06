b30606619fbc95eb9de658b0fe6960df2b73c276
Denis Reznikov <denis.reznikov@gmail.com>
2020-12-07 01:56:23 +0300

    create main handler for eat

--- scr/handlers/handler_eat.py
+++ scr/handlers/handler_eat.py
@@ -1,3 +1,13 @@
 from telegram import Update
 from telegram.ext import (CommandHandler, MessageHandler, Filters,
                           ConversationHandler, CallbackQueryHandler, CallbackContext)
+
+
+
+def eat_handler():
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('eat', do_eat)],
+        states={CHOOSING: [CallbackQueryHandler(button)]},
+        fallbacks=[MessageHandler(Filters.location, do_done)]
+    )
+    return conv_handler
