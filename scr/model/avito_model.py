481254d40fbe927ca66d54e52f15b9c639ef0b9c
Yurii <48685208+Endocriser@users.noreply.github.com>
2020-12-07 04:57:05 +0300

    Added time parsing.

--- scr/model/avito_model.py
+++ scr/model/avito_model.py
@@ -1,3 +1,5 @@
+import datetime
+
 import requests
 
 
@@ -25,3 +27,50 @@ class AvitoParser:
         print(url)
         r = self.session.get(url, params=params)
         return r.text
+
+    @staticmethod
+    def parse_date(item):
+        params = item.strip().split(' ')
+        # print(params)
+        if len(params) == 2:
+            day, time = params
+            if day == 'Сегодня':
+                date = datetime.date.today()
+            elif day == 'Вчера':
+                date = datetime.date.today() - datetime.timedelta(days=1)
+            else:
+                print('Не смогли разобрать день:', item)
+                return
+
+            time = datetime.datetime.strptime(time, '%H:%M').time()
+            return datetime.datetime.combine(date=date, time=time)
+
+        elif len(params) == 3:
+            day, month_hru, time = params
+            day = int(day)
+            months_map = {
+                'января': 1,
+                'февраля': 2,
+                'марта': 3,
+                'апреля': 4,
+                'мая': 5,
+                'июня': 6,
+                'июля': 7,
+                'августа': 8,
+                'сентября': 9,
+                'октября': 10,
+                'ноября': 11,
+                'декабря': 12,
+            }
+            month = months_map.get(month_hru)
+            if not month:
+                print('Не смогли разобрать месяц:', item)
+                return
+
+            today = datetime.datetime.today()
+            time = datetime.datetime.strptime(time, '%H:%M')
+            return datetime.datetime(day=day, month=month, year=today.year, hour=time.hour, minute=time.minute)
+
+        else:
+            print('Не смогли разобрать формат:', item)
+            return
