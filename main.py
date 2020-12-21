a9f3b84ab8c6a18a009d80cddb0c32ea8530cf60
Denis <Denis.reznikov@gmail.com>
2020-12-21 13:32:58 +0300

    finish with main file

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from telegram import Update, KeyboardButton, ReplyKeyboardMarkup
 from telegram.ext import Updater, CommandHandler
 from scr.handlers.handler_eat import eat_handler
-
+from scr.handlers.handler_weather import weather_handler
+from scr.handlers.avito_handler import avito_handler
 import os
 
 def do_start(update: Update, context):
-    #TODO: перенести клавву в другой класс
+    # TODO: перенести клавву в другой класс
     location_keyboard = [[KeyboardButton("Send location", request_location=True), ], ]
     keyboard = ReplyKeyboardMarkup(
         keyboard=location_keyboard,
@@ -22,12 +23,17 @@ def do_start(update: Update, context):
     update.message.reply_text(text=answer, reply_markup=keyboard)
 
 
+
 def main():
     TOKEN =  ['TELEGRAM_TOKEN']
     updater = Updater("790323839:AAGCpqOp4LXWd3O0DNYem32FyzF-32kRyGk", use_context=True)
     updater.dispatcher.add_handler(CommandHandler("start", do_start))
+    updater.dispatcher.add_handler(eat_handler())
+    updater.dispatcher.add_handler(weather_handler())
+    updater.dispatcher.add_handler(avito_handler())
     updater.start_polling()
     updater.idle()
+
 
 
 if __name__ == '__main__':
